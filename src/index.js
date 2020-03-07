db2c2a765ca59789aa3986acb1b1ddaef1e4e056
ardier16 <ardier16@gmail.com>
2020-03-07 18:35:28 +0200

    Use news api

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom'
 import App from 'components/App'
 import './index.css'
 
+import { initApi } from './api'
+import { config } from 'config'
+
+initApi(config.API_KEY)
+
 ReactDOM.render(<App />, document.getElementById('root'))
